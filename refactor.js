ffd42ec4e0b35559edeac1a3fb7d2c7d3606a76c
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-11 10:10:10 -0300

    refactor: replace temp with query playFor

--- refactor.js
+++ refactor.js
@@ -8,7 +8,8 @@ function statement(invoice, plays) {
   const format = new Intl.NumberFormat('en-US', { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
+    //replace temp with query
+    const play = playFor(perf)
 
     //extract function
     let thisAmount = amountFor(play, perf);
@@ -21,27 +22,31 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`
   return result
 
-  function amountFor(play, perf) {
-    let thisAmount = 0;
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(play, aPerformance) {
+    let result = 0;
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
 
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-    return thisAmount;
+    return result;
   }
 }
 
