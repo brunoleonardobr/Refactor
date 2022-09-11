0bbe6cefd04ce9727e30c43a5f7f7ec52bf51b0a
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-11 10:14:29 -0300

    refactor: inline variable

--- refactor.js
+++ refactor.js
@@ -25,7 +25,7 @@ function statement(invoice, plays) {
 
   function amountFor(play, aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -41,7 +41,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
