bf477a808dff2e644e9b203478bfb7b5cfe1fc69
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-11 10:05:55 -0300

    refactor: extract function amountFor

--- refactor.js
+++ refactor.js
@@ -9,25 +9,9 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0;
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
 
-        }
-        thisAmount += 300 * perf.audience
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
+    //extract function
+    let thisAmount = amountFor(play, perf);
     volumeCredits += Math.max(perf.audience - 30, 0)
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
     result += `${play.name}:${format(thisAmount / 100)} (${perf.audience} seats)\n`
@@ -36,6 +20,29 @@ function statement(invoice, plays) {
   result += `Amount owrd is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
+
+  function amountFor(play, perf) {
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+  }
 }
 
 console.log(statement(invoice, plays))
