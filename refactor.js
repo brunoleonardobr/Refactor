233535b1c0c1837977e71a3587372f81f4131c44
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-13 14:48:26 -0300

    refactor: extract function format

--- refactor.js
+++ refactor.js
@@ -5,7 +5,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat('en-US', { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
   for (let perf of invoice.performances) {
     //extract function, replace temp with query, inline variable
@@ -16,6 +15,10 @@ function statement(invoice, plays) {
   result += `Amount owrd is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
+  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0
