e269c88afca71b3df6563ab160ddcc001a21e8fb
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-13 19:00:08 -0300

    refactor: extract function and replace temp with query

--- refactor.js
+++ refactor.js
@@ -12,14 +12,19 @@ function statement(invoice, plays) {
   }
 
   //split loop - slide statements
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
-  result += `Amount owrd is ${usd(totalAmount)}\n`
+  result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
