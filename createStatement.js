002173c90fab87ed82a698b91de6f76af0b694cf
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-14 09:52:16 -0300

    refactor: create class calculator

--- createStatement.js
+++ createStatement.js
@@ -7,10 +7,11 @@ export default function createStatementData(invoice, plays) {
   return statementData
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
@@ -19,26 +20,7 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case "comedy":
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`)
-    }
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -55,5 +37,43 @@ export default function createStatementData(invoice, plays) {
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+      case "comedy":
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+
+        }
+        result += 300 * this.performance.audience
+        break
+      default:
+        throw new Error(`unknown type: ${this.play.type}`)
+    }
+    return result
+  }
+
+  get volumeCredits() {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5)
+    return result
   }
 }
