c5d49f5e72b3525fd4da04d18db959e7df4c4077
Bruno Leonardo Fagundes <brunoleonardodev@gmail.com>
2022-09-13 21:27:31 -0300

    refactor: add enrichPerformance

--- refactor.js
+++ refactor.js
@@ -6,17 +6,22 @@ function statement(invoice, plays) {
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformance)
   return renderPlainText(statementData, plays)
-}
 
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance)
-  return result
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(result)
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
 }
 
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`
   for (let perf of data.performances) {
-    result += `${playFor(perf).name}:${usd(amountFor(perf))} (${perf.audience} seats)\n`
+    result += `${perf.play.name}:${usd(amountFor(perf))} (${perf.audience} seats)\n`
   }
 
   result += `Amount owed is ${usd(totalAmount())}\n`
@@ -46,18 +51,14 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5)
     return result
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-  }
-
   function amountFor(aPerformance) {
     let result = 0
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000
         if (aPerformance.audience > 30) {
@@ -73,7 +74,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+        throw new Error(`unknown type: ${aPerformance.play.type}`)
     }
     return result
   }
